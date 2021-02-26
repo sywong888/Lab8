75400551b04656076c783f169f801d430edbdbd3
sywong888 <56745380+sywong888@users.noreply.github.com>
2021-02-26 00:34:27 -0800

    add tests 1 and 3

--- Part2-Cypress/cypress/integration/party_horn/custom-tests.js
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -1,9 +1,62 @@
 describe('Party Horn Tests', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:5500/');
+    cy.visit('http://127.0.0.1:5500/Part2-Cypress');
   });
 
   it('First Test', () => {
     expect(true).to.equal(true);
+  });
+
+  it('Slider changes when volume input changes', () => {
+    cy.get('#volume-number').clear().type('75');
+    cy.get('#volume-slider').then(($el) => {
+      expect($el).to.have.value(75);
+    })
+  });
+
+  it('Volume input changes when slider changes', () => {
+    cy.get('#volume-slider').invoke('val', 33).trigger('input');
+    cy.get('#volume-number').then(($el) => {
+      expect($el).to.have.value(33);
+    })
+  });
+
+  it('Volume of <audio> element changes when slider changes', () => {
+    cy.get('#volume-slider').invoke('val', 33).trigger('input');
+    cy.get('#horn-sound').then(($el) => {
+      expect($el).to.have.prop('volume', .33);
+    })
+  });
+
+  // first test requirement
+  it('Image source changes when you select the party horn radio button', () => {
+    cy.get('#radio-party-horn').click();
+    cy.get('#sound-image').then(($el) => {
+      expect($el).to.have.attr('src', './assets/media/images/party-horn.svg');
+    })
+  });
+
+  // first test requirement
+  it('Sound source changes when you select the party horn radio button', () => {
+    cy.get('#radio-party-horn').click();
+    cy.get('#horn-sound').then(($el) => {
+      expect($el).to.have.attr('src', './assets/media/audio/party-horn.mp3');
+    })
+  });
+
+  // third test requirement
+  it('Honk button is disabled when textbox input is empty', () => {
+    cy.get('#volume-number').clear();
+    cy.get('#honk-btn').then(($el) => {
+      expect($el).to.have.prop('disabled', true);
+    })
+  });
+
+  // third test requirement
+  it('Honk button is disabled when textbox input is a non-number', () => {
+    cy.get('#volume-number').clear().type('a');
+    cy.get('#honk-btn').then(($el) => {
+      expect($el).to.have.prop('disabled', true);
+    })
   });
 });
